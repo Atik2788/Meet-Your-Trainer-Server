533faaeb60e5cca776ab7484492c9addbb80c4f7
Atik2788 <ashanto653.2@gmail.com>
2022-11-08 12:32:48 +0600

    after doing post add courses

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 const express = require('express')
 const app = express()
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 
 // middleWare
 app.use(cors())
 app.use(express.json())
 
-// dbUser: practice-01
-// dbPass: AhZS2EHb1XSOz4op
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8ev4byy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const servicesCollection = client.db('trainerMongoCrud').collection('services')
+
+        // get data from mongo db.
+        app.get('/services', async(req, res)=>{
+           const query = {}
+           const cursor = servicesCollection.find(query)
+           const service = await cursor.toArray()
+           res.send(service)
+        })
+
+
+        
+        // Post data in mongodb
+        app.post('/services', async(req, res)=>{
+            const services = req.body;
+            const result = await servicesCollection.insertOne(services)
+            res.send(result)
+            console.log(services)
+        })
+       
+     
+
+
+       
+        // post or create (CRUD) ***************** POST, CREATE ************
+
+
+
+        // Delete (CRUD) *********** Deete  ***************
+
+
+
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.log(err))
+
 
 
 
