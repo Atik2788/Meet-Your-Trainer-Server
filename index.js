e3b88a9bd3c513c4cbd11f8e75dd086a6ccd36dd
Atik2788 <ashanto653.2@gmail.com>
2022-11-09 20:06:12 +0600

    after send and get review

--- index.js
+++ index.js
@@ -1,82 +1,104 @@
-const express = require('express')
-const app = express()
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const { query } = require("express");
 const port = process.env.PORT || 5002;
 
 // middleWare
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8ev4byy.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const servicesCollection = client
+      .db("trainerMongoCrud")
+      .collection("services");
+    const reviewCollection = client
+      .db("trainerMongoCrud")
+      .collection("reviews");
+
+    // get data from mongo db.
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const service = await cursor.toArray();
+      res.send(service);
+    });
+
+    // get data from mongo db by limit.
+    app.get("/serviceslimit", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const service = await cursor.limit(3).toArray();
+      res.send(service);
+    });
+
+    // get data by id
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.send(service);
+    });
+
+    // Post services data in mongodb
+    app.post("/services", async (req, res) => {
+      const services = req.body;
+      const result = await servicesCollection.insertOne(services);
+      res.send(result);
+      console.log(services);
+    });
 
 
-async function run(){
-    try{
-        const servicesCollection = client.db('trainerMongoCrud').collection('services')
-
-        // get data from mongo db.
-        app.get('/services', async(req, res)=>{
-           const query = {}
-           const cursor = servicesCollection.find(query)
-           const service = await cursor.toArray()
-           res.send(service)
-        })
 
 
-        // get data from mongo db.
-        app.get('/serviceslimit', async(req, res)=>{
-           const query = {}
-           const cursor = servicesCollection.find(query)
-           const service = await cursor.limit(3).toArray()
-           res.send(service)
-        })
+    // get reviews
+    app.get("/reviews", async (req, res) => {
+        // console.log(req.query.serviceId)
+      let query = {};
+      if(req.query.serviceId){
+        query ={
+            serviceId: req.query.serviceId
+        }
+      }
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
 
-
-        
-        // Post data in mongodb
-        app.post('/services', async(req, res)=>{
-            const services = req.body;
-            const result = await servicesCollection.insertOne(services)
-            res.send(result)
-            console.log(services)
-        })
-       
-     
-
-
-       
-        // post or create (CRUD) ***************** POST, CREATE ************
+    
 
 
 
-        // Delete (CRUD) *********** Deete  ***************
+    // Post reviews Data in mongodb
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      console.log(review);
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+      console.log("reviews is running", review);
+    });
 
-
-
-    }
-    finally{
-
-    }
+  } finally {
+  }
 }
-run().catch(err => console.log(err))
 
 
+run().catch((err) => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-
-
-app.get('/', (req, res)=>{
-    res.send('Hello world')
-})
- 
-
-
-app.listen(port, () =>{
-    console.log(`app running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`app running on port: ${port}`);
+});
